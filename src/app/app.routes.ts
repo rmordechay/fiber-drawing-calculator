199e12bf71858489da788f4d49e9f8d48e0dd063
walt-grace <roi3363@yahoo.com>
2024-05-24 14:29:22 +0200

    Deployed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {CrystalComponent} from "./crystal/crystal.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CrystalComponent },
+    { path: '**', component: CrystalComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
